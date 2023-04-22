642e2974cd772b6fd8b2fc9ac5b7e04c60941d7b
Ekaterina <katerina331-441@ya.ru>
2023-04-22 17:46:52 +0300

    Commit TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void CashBackMax() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 800;
+        int actual = service.remain(1200);
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void CashBackMin() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 200;
+        int actual = service.remain(800);
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void CashBackNon() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(actual,expected);
+    }
+}
